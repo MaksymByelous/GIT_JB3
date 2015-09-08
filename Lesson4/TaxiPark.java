f35985be9b2a80d873c17941b595b57f65591efb
Max <MaxwellBels@gmail.com>
2015-09-08 13:59:06 +0300

    next

--- Lesson4/TaxiPark.java
+++ Lesson4/TaxiPark.java
@@ -4,31 +4,41 @@ package Lesson4;
  * Created by Max on 07.09.2015.
  */
 class TaxiPark {
+    int cars[];
 }
 
 class Car {
-    int passenger, fuelneed;
+    int passenger;
+    int fuelneed;
+    int number;
 
-    public Car(int passenger, int fuelneed) {
+    public Car(int passenger, int fuelneed, int number) {
         this.passenger = passenger;
         this.fuelneed = fuelneed;
+        this.number = number;
+    }
+}
+
+class Taxi extends Car {
+    public Taxi(int passenger, int fuelneed, int number) {
+        super(passenger, fuelneed, number);
     }
 }
 
 class Minivan extends Car {
-    public Minivan(int passenger, int fuelneed) {
-        super(passenger, fuelneed);
+    public Minivan(int passenger, int fuelneed, int number) {
+        super(passenger, fuelneed, number);
     }
 }
 
 class Track extends Car {
-    public Track(int passenger, int fuelneed) {
-        super(passenger, fuelneed);
+    public Track(int passenger, int fuelneed, int number) {
+        super(passenger, fuelneed, number);
     }
 }
 
 class Cabrio extends Car {
-    public Cabrio(int passenger, int fuelneed) {
-        super(passenger, fuelneed);
+    public Cabrio(int passenger, int fuelneed, int number) {
+        super(passenger, fuelneed, number);
     }
 }
