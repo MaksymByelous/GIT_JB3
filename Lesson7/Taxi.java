0a78f32a981c375427b9a9561f299e985ab59387
Max <MaxwellBels@gmail.com>
2015-09-24 13:26:28 +0300

    next

---
+++ Lesson7/Taxi.java
@@ -0,0 +1,15 @@
+package Lesson7;
+
+/**
+ * Created by Max on 08.09.2015.
+ */
+public class Taxi extends Car {
+    public Taxi(int passenger, int fuelconsumption, int number) {
+        super(passenger, fuelconsumption, number);
+    }
+
+    @Override
+    void ShowNumber() {
+        super.ShowNumber();
+    }
+}
---
+++ Lesson7/Track.java
@@ -0,0 +1,15 @@
+package Lesson7;
+
+/**
+ * Created by Max on 08.09.2015.
+ */
+public class Track extends Car {
+    public Track(int passenger, int fuelconsumption, int number) {
+        super(passenger, fuelconsumption, number);
+    }
+
+    @Override
+    void ShowNumber() {
+        super.ShowNumber();
+    }
+}
