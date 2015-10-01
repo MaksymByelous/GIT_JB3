95006af07107bc60485a27482c21892c30eaba06
Max <MaxwellBels@gmail.com>
2015-10-01 18:48:47 +0300

    next

--- Lesson9/Main.java
+++ Lesson9/Main.java
@@ -2,17 +2,12 @@ package Lesson9;
 
 import java.util.ArrayList;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         Taxi taxi = new Taxi(4, 10, 3458, "empty");
         Cabrio cabrio = new Cabrio(2, 14, 8264, "full");
         Track track = new Track(2, 25, 1111, "empty");
         Minivan minivan = new Minivan(8, 16, 5861, "empty");
-        Car taxipark[] = new Car[100];
-        taxipark[0] = taxi;
-        taxipark[1] = cabrio;
-        taxipark[2] = track;
-        taxipark[3] = minivan;
 
         ArrayList<Car> taxiList = new ArrayList<>();
         taxiList.add(taxi);
@@ -21,6 +16,7 @@ public class Main {
         taxiList.add(minivan);
 
         System.out.println("Taxi park contains " + taxiList.size() + " cars:");
+        System.out.println(" Car     |" +"|  Number  |" +"| Passengers |" +"| Lietrs/100km |"+"|   Tank  |");
         taxiList.forEach(Lesson9.Car::carInfo);
 
         System.out.print('\n');
@@ -35,7 +31,6 @@ public class Main {
                 System.out.print(elementOfArray.getClass().getSimpleName());
                 e.printStackTrace();
             }
-            continue;
         }
     }
 }
